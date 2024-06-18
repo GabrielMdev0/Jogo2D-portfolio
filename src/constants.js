a4b83c3273b09802edb481401dd9edac53639062
gabriel <gabriel.mendes.developer@gmail.com>
2024-06-18 15:41:25 -0300

    projeto pronto

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Esse e meu computador. Trabalho atualmente com HTML, CSS, JAVA e JavaScript.
+    Nao importa a linguagem, eu apenas gosto de programar.
+    Esse e meu <a href="https://github.com/GabrielMdev0" target="_blank">Github</a>!`,
+  "cs-degree": `Esse e meu certificado de FullStack. esta na parede porque tenho orgulho dele.`,
+  "sofa-table": `esse e meu sofa. onde eu relaxo e assisto  youtube. `,
+  tv: `Essa e minha TV. onde assisto youtubers de tecnologia que eu gosto muito :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (As vezes) e <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `Aqui e onde eu durmo. Grandes ideias surgem quando estou deitado nela.`,
+  resume: `Essa e minha mesa e esse e meu curriculum. <a href="https://github.com/GabrielMdev0" target="_blank">Confira!</a>
+  Contate-me no gabriel.mendes.developer@gmail.com se voce tiver algum interesse em oportunidades de trabalho!`,
+  projects: `informacoes sobre este portfolio: E feito com a biblioteca Kaboom.js que e uma biblioteca para fazer jogos em JavaScript.
+  O texto e renderizado com HTML/CSS. Portanto, a caixa de texto que voce esta lendo atualmente nao e renderizada no canvas.`,
+  library: `Existem varios livros sobre tecnologia na minha prateleira. alguns em ingles (tambem falo ingles).
+  claro que nao li todos. Quem mais compra compulsivamente livros tecnicos sem nunca termina-los?`,
+  exit: `se voce quer sair do portfolio, e so fechar a aba.`,
+};
